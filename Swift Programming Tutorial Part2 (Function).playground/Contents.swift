9223354d9a0e9f60e1fd6ab054ef3cb4ca78ba12
wangyuanhsiang <ywang91@wb316-mac01.local>
2015-12-17 17:54:50 -0700

    Add New - Shawn 2015/12/17

--- Swift Programming Tutorial Part2 (Function).playground/Contents.swift
+++ Swift Programming Tutorial Part2 (Function).playground/Contents.swift
@@ -21,3 +21,34 @@ func sayHelloTo(name: String) -> String{
 
 
 sayHelloTo("Shawn")
+
+
+func sum (a:Int, b:Int) -> Int{
+    return a + b
+}
+
+var total = sum( 14, b: 16)
+
+func sumAndCeiling(a: Int, b:Int) -> (Int,Int){
+    var ceiling = a > b ? a : b
+    var sum  = a + b
+    return(sum, ceiling)
+}
+
+var CeilingResult = sumAndCeiling(14, b: 52)
+
+CeilingResult.0
+CeilingResult.1
+
+
+func sumAndFloor(a: Int, b:Int) -> (sum: Int, floor: Int){
+    var floor = a < b ? a : b
+    var sum = a + b
+    return(sum,floor)
+}
+
+var floorTuple = sumAndFloor(13, b: 4)
+floorTuple.sum
+floorTuple.floor
+
+//end at 14:57
